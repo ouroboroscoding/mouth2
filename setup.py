7646ac2b2f065e916f9459c585cd6d91dfaf57ad
Ouroboros Coding Inc <chris@ouroboroscoding.com>
2024-12-31 12:36:48 -0500

    Hotfix 2.0.2 - Updated version - Updated brain2-oc to 2.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as oF:
 
 setup(
 	name='mouth2-oc',
-	version='2.0.1',
+	version='2.0.2',
 	description='Mouth contains a service to run outgoing communications like email and sms messages',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
